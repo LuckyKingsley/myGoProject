98f8338edcdbfe9d20e0e2c8effa9f1ea331fc6e
jinxin <qingtongcs>
2019-09-04 18:07:37 +0800

    190904 18：07

--- geometry.go
+++ geometry.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"geometry/rectangle"
+	"geometry/myMap"
 	"log"
 	_ "math"
-	"geometry/slice"
-	"geometry/base"
 )
 
 //在Go中，任何以大写字母开头的变量或者函数都是被导出的名字。其它包只能访问被导出的函数和变量
 
 /*
  * 1. 包级别变量
-*/
+ */
 var rectLen, rectWidth float64 = 6, 7
+
 //var _ = math.Min // 错误屏蔽器
 
 /*
 *2. init 函数会检查长和宽是否大于0
-*/
+ */
 func init() {
 	println("main package initialized")
 	if rectLen < 0 {
@@ -31,20 +29,19 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Geometrical shape properties")
-	/*Area function of rectangle package used*/
-	fmt.Printf("area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth))
-	/*Diagonal function of rectangle package used*/
-	fmt.Printf("diagonal of the rectangle %.2f ", rectangle.Diagonal(rectLen, rectWidth))
+	//fmt.Println("Geometrical shape properties")
+	///*Area function of rectangle package used*/
+	//fmt.Printf("area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth))
+	///*Diagonal function of rectangle package used*/
+	//fmt.Printf("diagonal of the rectangle %.2f ", rectangle.Diagonal(rectLen, rectWidth))
+	//
+	//if num := 10; num%2 == 0 { //checks if number is even
+	//	fmt.Println(num, "is even")
+	//} else {
+	//	fmt.Println(num, "is odd")
+	//}
 
-	if num := 10; num%2 == 0 { //checks if number is even
-		fmt.Println(num, "is even")
-	} else {
-		fmt.Println(num, "is odd")
-	}
-
-	base.Base()
-	slice.ArraySlice()
-
+	//base.Base()
+	//slice.ArraySlice()
+	myMap.MyMap()
 }
-
---
+++ myMap/myMap.go
@@ -0,0 +1,48 @@
+package myMap
+
+import "fmt"
+
+func MyMap() {
+	// map 的零值是 nil。
+	// 如果你想添加元素到 nil map 中，会触发运行时 panic。因此 map 必须使用 make 函数初始化。
+	//personSalary := make(map[string]int)
+	var map1 map[string]int
+	if map1 == nil {
+		fmt.Println("map is nil. Going to make one.")
+		map1 = make(map[string]int)
+	}
+	map1["a"] = 123123
+	map1["b"] = 123123312
+	map1["c"] = 151235
+
+	map2 := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	map2["mike"] = 9000
+	fmt.Println("map contents:", map2)
+	fmt.Println("map contents:", map2["mike"])
+	//判断是否存在key
+	value, ok := map2["mike"]
+	if ok == true {
+		fmt.Println("mike", "is", value)
+	} else {
+		fmt.Println("mike not found")
+	}
+	for key, value := range map2 {
+		fmt.Printf("map2[%s] = %d\n", key, value)
+	}
+	delete(map2, "mike")
+	fmt.Println(len(map2))
+
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("Original person salary", personSalary)
+	newPersonSalary := personSalary
+	newPersonSalary["mike"] = 18000
+	fmt.Println("Person salary changed", personSalary)
+
+}
